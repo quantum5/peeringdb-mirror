2fa79c7782652d0895c03811d699afe467c123ab
Quantum <quantum2048@gmail.com>
2024-04-12 22:45:11 -0400

    Fix settings path in WSGI and ASGI files

--- peeringdb_mirror/asgi.py
+++ peeringdb_mirror/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peeringdb_mirror.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peeringdb_mirror.settings.local')
 
 application = get_asgi_application()
